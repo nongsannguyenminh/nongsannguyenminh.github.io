f155600059f7de1bcb8df1e4f68bcdda8c109d20
Nguyễn Tấn Dũng <tandung251298@gmail.com>
2020-04-12 19:10:13 +0700

    add

--- _site/js/main.js
+++ _site/js/main.js
@@ -215,4 +215,23 @@
         4: "The username portion of the email address is invalid (the portion before the @: )",
         5: "This email address looks fake or invalid. Please enter a real email address",
     }
+    $(document).on("click", 'a[href^="#"]', function(e) {
+        // target element id
+        var id = $(this).attr("href")
+
+        // target element
+        var $id = $(id)
+        if ($id.length === 0) {
+            return
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault()
+
+        // top position relative to the document
+        var pos = $id.offset().top
+
+        // animated top scrolling
+        $("body, html").animate({ scrollTop: pos })
+    })
 })(jQuery)
